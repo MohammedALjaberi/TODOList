d45d147fccd367afdbef0e816e7d9a3d861f251a
MohammedAljaberi <aljabri606@gmail.com>
2024-08-26 13:34:49 +0300

    add validation to form component

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,23 +1,8 @@
 import textArea from '/src/styles/TextArea.module.css'
 import Filter from './Filter'
 import searchIcon from '../assets/HugeiconsSearch01.svg'
-// import { useState } from 'react'
-// import { Task } from './Form'
-
-// type task = Task['task']
-
-// const getFilteredItems =(query:string , items : task) => {
-//   if(!query)  {
-//     return items
-//   }
-// }
 
 const SearchField = () => {
-  // const[query , setQuery] = useState('')
-  // const tracks = task
-  // const items = tracks
-  // const filteredItems = getFilteredItems(query,items)
-
   return (
     <div className={textArea.inpunt_container}>
       <div className={textArea.search_cont}>
